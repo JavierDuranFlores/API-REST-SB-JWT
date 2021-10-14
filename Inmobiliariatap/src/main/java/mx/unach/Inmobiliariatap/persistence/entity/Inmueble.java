6380d2e453c6994caaad3fa38752f25eaec1d3c7
Javier Duran Flores <duranfloresj7@gmail.com>
2021-10-14 01:33:30 -0500

    Creamos la entidad Inmueble que mappearemos con la tabla inmuebles de la base de datos

---
+++ Inmobiliariatap/src/main/java/mx/unach/Inmobiliariatap/persistence/entity/Inmueble.java
@@ -0,0 +1,52 @@
+package mx.unach.Inmobiliariatap.persistence.entity;
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name="inmuebles")
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString	
+public class Inmueble implements Serializable {
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+    @Column(name="idc")
+	@Getter @Setter
+    private String idcliente;
+
+    @Column(name="municipio")
+    @Getter @Setter
+    private String municipio;
+
+    @Column(name="colonia")
+    @Getter @Setter
+    private String colonia;
+
+    @Column(name="codigopostal")
+    @Getter @Setter
+    private String codigopostal;
+
+    @Column(name="precio")
+    @Getter @Setter
+    private Double precio;
+
+    @Column(name="imagen")
+    @Getter @Setter
+    private String imagen;
+    
+}
